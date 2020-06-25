5e18e5667b6115cb52e604950a545837e0e9329c
Muhammad Arsalan <56469631+MuhammadArsalan99@users.noreply.github.com>
2020-06-25 09:00:06 -0700

    Adding Admin.java for Expenses Module

---
+++ src/dental_management_system/Admin.java
@@ -0,0 +1,144 @@
+package dental_management_system;
+
+import java.sql.ResultSet;
+
+public class Admin extends User
+{
+
+    public Admin(String userName, String password, String userType)
+    {
+        super(userName, password, userType);
+    }
+     
+
+   public ResultSet ShowAllMedicnesRecord()
+   {
+        String query="SELECT * FROM medicinesRecord";
+        ResultSet rs=null;
+        DataBase_Connection  conn = new DataBase_Connection ();
+        
+        try
+        {
+        
+        
+        conn.MakeConnection();
+        
+        rs = conn.RunSelectQuery(query);
+
+     
+        }
+        catch (Exception e)
+        {
+                
+        }
+        
+        return rs;
+    }
+   public ResultSet ShowAllEquipmentRecord()
+   
+    {
+        
+        String query="SELECT * FROM equipmentsRecords";
+        ResultSet rs=null;
+        DataBase_Connection  conn = new DataBase_Connection ();
+        
+        try
+        {
+        
+        
+        conn.MakeConnection();
+        
+        rs = conn.RunSelectQuery(query);
+
+     
+        }
+        catch (Exception e)
+        {
+                
+        }
+        
+        return rs;
+  
+    }
+   public ResultSet ShowAllExpensesRecord()
+   
+    {
+        
+        String query="SELECT * FROM expensesRecord";
+        ResultSet rs=null;
+        DataBase_Connection  conn = new DataBase_Connection ();
+        
+        try
+        {
+        
+        
+        conn.MakeConnection();
+        
+        rs = conn.RunSelectQuery(query);
+
+     
+        }
+        catch (Exception e)
+        {
+                
+        }
+        
+        return rs;
+  
+    }
+   public void InsertExpensesData(Expenses expenses)
+    {
+ 
+     String query = "INSERT INTO expensesRecord (Month, Payments, Electricity_Bills, Water_Bills, Maintenance_expenses_of_Dental_Units)"
+        + "VALUES ('"+expenses.month+"', '"+expenses.payments+"', '"+expenses.electricityBills+"' , '"+expenses.waterBills+"', '"+expenses.maintenance+"')";
+        
+        
+     DataBase_Connection  conn = new DataBase_Connection ();
+     
+        try
+        {
+           conn.MakeConnection();
+
+           conn.RunInsertQuery(query);
+
+           
+        }
+        catch (Exception e)
+        {
+
+        }
+        finally
+        {
+            try
+            {
+                conn.CloseConnection();
+            }
+            catch (Exception e)
+            {
+                        
+            }
+        }
+        
+}
+   public ResultSet SearchExpensesData(String text)
+    {
+        String query = "SELECT * FROM expensesRecord WHERE Month LIKE '%"+text+"%'";
+        ResultSet rs=null;
+        
+        DataBase_Connection  conn = new DataBase_Connection ();
+        
+        try{            
+          
+            conn.MakeConnection();
+            
+            rs = conn.RunSelectQuery(query);
+
+            }     
+            catch (Exception e) 
+            {
+            
+            } 
+            
+        return rs;
+    }
+}
